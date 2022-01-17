4617f623b1d6cbc4cd928767678f65b9d5bf3b4d
Sam French <samfrench@gmail.com>
2022-01-17 14:12:57 -0600

    draw cube

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import * as THREE from 'three';
 
-function component() {
-  const element = document.createElement('div');
-  
-  return element;
-}
-
-document.body.appendChild(component());
+//https://threejs.org/examples/#webgl_loader_gltf
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -15,19 +9,17 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
-
 camera.position.z = 5;
-
 function animate() {
 	requestAnimationFrame( animate );
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
+	
 	renderer.render( scene, camera );
 };
 
