3f6ba0ccf8632b56e8a90c1ce7c4fedbe4757a65
Sam French <samfrench@gmail.com>
2022-01-17 22:20:17 -0600

    play around with gltf model

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 
 //https://threejs.org/examples/#webgl_loader_gltf
+//https://threejs.org/editor/
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color( 0xbfe3dd );
@@ -13,13 +15,13 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const light = new THREE.AmbientLight( 0x404040 , 10); // soft white light
+const light = new THREE.AmbientLight( 0x404040 , 4); // soft white light
 scene.add( light );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshLambertMaterial( {color: 0xffffff} );
 const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+//scene.add( cube );
 //camera.position.z = 5;
 camera.position.set( 5, 2, 8 );
 
@@ -31,10 +33,13 @@ const dracoLoader = new DRACOLoader();
 dracoLoader.setDecoderPath('https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/js/libs/draco/'); // use a full url path
 loader.setDRACOLoader(dracoLoader);
 
-loader.load( 'cactus.glb', function ( gltf ) {
+loader.load( 'cactus.gltf', function ( gltf ) {
 
 	const model = gltf.scene;
 	//model.position.set( 0, 0, 0 );
+	
+	//https://threejs.org/docs/#api/en/loaders/TextureLoader
+	
 	model.scale.set( 10, 10, 10 );
 	scene.add( model );
 	console.log('added');
@@ -45,3 +50,51 @@ loader.load( 'cactus.glb', function ( gltf ) {
 	console.error( error );
 
 } );
+
+/*
+loader.load( 'Cactus3.glb', function ( gltf ) {
+
+	const model = gltf.scene;
+	//model.position.set( 0, 0, 0 );
+
+	scene.add( model );
+	console.log('added');
+	renderer.render(scene,camera);
+
+}, undefined, function ( error ) {
+	
+	console.error( error );
+
+} );
+
+// instantiate a loader
+const objLoader = new OBJLoader();
+
+// load a resource
+objLoader.load(
+	// resource URL
+	'cactus2.obj',
+	// called when resource is loaded
+	function ( object ) {
+
+		object.position.set( 0, 0, 0 );
+
+		scene.add( object );
+		renderer.render(scene,camera);
+		console.log('loaded obj');
+
+	},
+	// called when loading is in progresses
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
+*/
